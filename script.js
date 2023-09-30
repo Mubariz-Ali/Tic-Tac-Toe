786fa9bac33fb74364d09a1753714c17a7cfff8f
Mubariz Ali <mirzamuhammadmubariz@gmail.com>
2023-09-30 14:51:45 +0500

    Tic Tac Toe Game Creation Complete

--- script.js
+++ script.js
@@ -19,8 +19,9 @@ let win = [
 ]
 win.forEach(e => {
     if((boxtext[e[0]].innerText === boxtext[e[1]].innerText) && (boxtext[e[2]].innerText === boxtext[e[1]].innerText) && (boxtext[e[0]].innerText !== "")){
-        document.querySelector('.Info').innerText = boxtext[e[0]].innerText + "Won"
+        document.querySelector('.TurnInfo').innerText = boxtext[e[0]].innerText + " Won"
         gameover = true
+        document.querySelector('.ImageBox').getElementsByTagName('img')[0].style.width = "200px"
     }
 })
 }
@@ -38,4 +39,15 @@ Array.from(boxes).forEach(element => {
             }
         }
     })
+})
+let button = document.querySelector('.Reset')
+button.addEventListener('click', () => {
+    let boxtext = document.querySelectorAll('.boxtext');
+    Array.from(boxtext).forEach(element => {
+        element.innerText = ""
+    });
+    Turn = "X";
+    gameover = false
+    document.getElementsByClassName('TurnInfo')[0].innerText = "Turn for " + Turn;
+    document.querySelector('.ImageBox').getElementsByTagName('img')[0].style.width = "0px"
 })
